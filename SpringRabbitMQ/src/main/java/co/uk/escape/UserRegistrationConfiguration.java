4d0b9d643fca6df387112f26892df69f4cbf8a6a
Alex Theedom <alex.theedom@gmail.com>
2014-04-28 22:29:37 +0100

    Added the GET method for getUserInfo method

--- SpringRabbitMQ/src/main/java/co/uk/escape/UserRegistrationConfiguration.java
+++ SpringRabbitMQ/src/main/java/co/uk/escape/UserRegistrationConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import co.uk.escape.web.RMQTemplate;
+
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
@@ -28,8 +30,14 @@ public class UserRegistrationConfiguration {
 	DirectExchange exchange() {
 		return new DirectExchange("user-registrations-exchange");
 	}
-		
+	
 	@Bean
+	DirectExchange exchangeUserInfo() {
+		return new DirectExchange("user-info-exchange");
+	}
+
+	// Create new user
+	@Bean @RMQTemplate(RMQTemplate.Type.CREATE_USER)
 	RabbitTemplate template(DirectExchange exchange, Queue replyQueue, ConnectionFactory connectionFactory){
 		Jackson2JsonMessageConverter jsonConverter = new Jackson2JsonMessageConverter();
 		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
@@ -49,4 +57,24 @@ public class UserRegistrationConfiguration {
         return container;
     }
 
+	// Get user info
+	@Bean @RMQTemplate(RMQTemplate.Type.GET_USER_INFO)
+	RabbitTemplate templateUserInfo(DirectExchange exchangeUserInfo, Queue replyQueue, ConnectionFactory connectionFactory){
+		Jackson2JsonMessageConverter jsonConverter = new Jackson2JsonMessageConverter();
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jsonConverter);
+		rabbitTemplate.setExchange(exchangeUserInfo.getName());
+		rabbitTemplate.setRoutingKey("user");
+		rabbitTemplate.setReplyQueue(replyQueue);
+		return rabbitTemplate;
+	}
+	
+    @Bean
+    public SimpleMessageListenerContainer replyUserInfoListenerContainer(RabbitTemplate templateUserInfo, Queue replyQueue, ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueues(replyQueue);
+        container.setMessageListener(templateUserInfo);
+        return container;
+    }
 }
--- SpringRabbitMQ/src/main/java/co/uk/escape/web/UserRegistrationController.java
+++ SpringRabbitMQ/src/main/java/co/uk/escape/web/UserRegistrationController.java
@@ -4,9 +4,11 @@ package co.uk.escape.web;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import co.uk.escape.domain.RegisteredUser;
 import co.uk.escape.domain.RegistrationRequest;
+import co.uk.escape.domain.UserInfoRequest;
 
 import org.springframework.http.MediaType;
 
@@ -21,22 +24,42 @@ import org.springframework.http.MediaType;
 @RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserRegistrationController {
 
-	@Autowired
-	RabbitTemplate rabbitTemplate;
+	@Autowired @RMQTemplate(RMQTemplate.Type.CREATE_USER)
+	RabbitTemplate rabbitTemplateUser;
+	
+	@Autowired @RMQTemplate(RMQTemplate.Type.GET_USER_INFO)
+	RabbitTemplate rabbitTemplateInfo;
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public RegisteredUser registerUser(@RequestBody RegistrationRequest newUserRegistrationRequest){
-		System.out.println("in the controller");
+		System.out.println("in the controller: registerUser()");
 		
-		RegisteredUser registeredUser = (RegisteredUser)rabbitTemplate.convertSendAndReceive(newUserRegistrationRequest);		
+		RegisteredUser registeredUser = (RegisteredUser)rabbitTemplateUser.convertSendAndReceive(newUserRegistrationRequest);		
 		
-		System.out.println("obj returned: " + registeredUser);		
+		System.out.println("obj returned from registerUser(): " + registeredUser);		
 		return registeredUser;
 	}
 
-
+	@RequestMapping(method = RequestMethod.GET, value = "/{emailAddress}")
+	public RegisteredUser getUserInfo(@PathVariable String emailAddress){
+		System.out.println("in the controller: getUserInfo()");
+		
+		UserInfoRequest userInfoRequest = new UserInfoRequest(emailAddress);
+		
+		RegisteredUser registeredUser = (RegisteredUser)rabbitTemplateInfo.convertSendAndReceive(userInfoRequest);		
+		
+		System.out.println("obj returned from getUserInfo(): " + registeredUser);		
+		return registeredUser;
+	}
+	
+	
 	@ExceptionHandler(DuplicateKeyException.class)
-	ResponseEntity<String> handleNotFounds(Exception e) {
+	ResponseEntity<String> duplicateKey(Exception e) {
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+	}
+	
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	ResponseEntity<String> handleNotFounds(Exception e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.GONE);
 	}
 }
